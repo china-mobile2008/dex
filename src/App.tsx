5f2941e24730497d7c7b5e5958a2696bd12a791c
Mr.Zhang <zhangkun@huoxing24.com>
2024-10-16 13:55:33 +0800

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -84,6 +84,10 @@ export const handleFormData = (config) => {
   const { provider, chainIds, feeConfig, tokenPair, ...reset } = config;
   const [providerType, providerFrom] = provider?.split('-') || [];
   let feeConfigObj = {
+    '56': {
+      feePercent: 1,
+      referrerAddress: '0xc93822354dc7fd3130c0821f95d7b04b4fadedc0',
+    },
     '137': {
       feePercent: 1,
       referrerAddress: '0xc93822354dc7fd3130c0821f95d7b04b4fadedc0',
